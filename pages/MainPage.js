2fac17f36d8fd633e781ae06e58a3420079794b7
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-12-01 15:13:41 +0100

    init tailwind + next

---
+++ pages/MainPage.js
@@ -0,0 +1,11 @@
+export default function MainPage() {
+  return (
+    <main>
+      <div className="font-bold text-3xl flex w-full items-center justify-center mt-6">
+        <h1 className="text-blue-500">
+          Okey Cards <span className="text-blue-500">Helper</span>
+        </h1>
+      </div>
+    </main>
+  );
+}
